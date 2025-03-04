dcbd1d3cfeba6944914146d4192be8a7f677fd95
rafabpg <rafa23giordano@gmail.com>
2025-03-04 18:36:48 -0300

    feat: adding config to aplication

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ export default {
   ],
   theme: {
     extend: {},
+    colors:{
+      "gray-900":"#121212",
+      "gray-800":"#1E1E1E",
+      "gray-200":"#EAEAEA",
+      "gray-400":"#B3B3B3",
+      "cyan-400":"#00A8E8",
+      "purple-900":"#8A2BE2",
+      "green-400":"#00FF7F",
+      "pink-500":"#FF007F"
+    }
   },
   plugins: [],
 }
